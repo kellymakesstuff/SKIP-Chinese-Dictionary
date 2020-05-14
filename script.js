33dd635ba6d1030b55f1ae6f8a504f1b97f09954
bergkma <bergkma@gmail.com>
2020-05-13 21:29:54 -0400

    added modal links and dynamic changing

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+let leftRightImg = document.getElementById("leftrightimg")
+let leftRightDiv = document.getElementById("left-right-div")
+let topBottomImg = document.getElementById("topbottomimg")
+let topBottomDiv = document.getElementById("top-bottom-div")
+let enclosedImg = document.getElementById("enclosedimg")
+let enclosedDiv = document.getElementById("enclosed-div")
+
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
@@ -17,6 +24,33 @@ span.onclick = function () {
   modal.style.display = "none";
 }
 
+leftRightImg.onclick = function () {
+  modal.style.display = "none";
+  leftRightMenu();
+  leftRightDiv.style.display = "block";
+  topBottomDiv.style.display = "none";
+  enclosedDiv.style.display = "none";
+  
+}
+
+topBottomImg.onclick = function () {
+  modal.style.display = "none";
+  topBottomMenu();
+  topBottomDiv.style.display = "block";
+  leftRightDiv.style.display = "none";
+  enclosedDiv.style.display = "none";
+ 
+}
+
+enclosedImg.onclick = function () {
+  modal.style.display = "none";
+  enclosedMenu();
+  enclosedDiv.style.display = "block";
+  leftRightDiv.style.display = "none";
+  topBottomDiv.style.display = "none";
+ 
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
@@ -24,9 +58,18 @@ window.onclick = function (event) {
   }
 }
 
+
+
 ////////
 
 
+function removeChars() {
+  let oldChar = document.querySelector("#character-population")
+  console.log('last child', oldChar.lastChild)
+  while (oldChar.lastChild) {
+    oldChar.removeChild(oldChar.lastChild)
+  }
+}
 
 let leftRight = [1, 3, 4, 5, 8, 14, 15, 17, 18, 20, 23, 24, 25, 27, 28, 29, 31, 32, 35, 37, 38, 40, 45, 46, 47, 48, 49]
 let topBottom = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 23, 24, 27, 28, 29, 31, 32, 33, 34, 35, 36, 38, 39, 40, 41, 44, 45, 46, 47, 48, 49]
@@ -75,49 +118,9 @@ let leftRightMenu = async () => {
     radicalPass(getValue)
   }
 
-  // addChars(selectForm.value)
+ 
 }
 
-///e.target event listener - look up with multiple inputs 
-
-
-
-
-leftRightMenu();
-
-let radicalPass = async (radicalnum) => {
-  let url = `http://ccdb.hemiola.com/characters/radicals/${radicalnum}?&fields=string,ktotalstrokes&filters=simplified`
-
-  try {
-    let response = await axios.get(url)
-
-    function addChars() {
-      for (i = 0; i < response.data.length; i++) {
-        let singleChar = document.createElement('p')
-        singleChar.innerText = response.data[i].string
-        document.querySelector('#character-population').append(singleChar)
-
-      }
-    }
-
-    function removeChars() {
-      let oldChar = document.querySelector("#character-population")
-      console.log('last child', oldChar.lastChild)
-      while (oldChar.lastChild) {
-        oldChar.removeChild(oldChar.lastChild)
-      }
-    }
-
-    addChars()
-    // removeChars()
-
-
-  } catch (error) {
-    console.log(`${error}`)
-  } finally {
-
-  }
-}
 
 let topBottomMenu = async () => {
   // let url = "http://ccdb.hemiola.com/characters?&fields=string,ktotalstrokes&filters=simplified&big5a"
@@ -134,12 +137,23 @@ let topBottomMenu = async () => {
       topBottomList.push(response.data[topBottom[i]].string)
       // console.log(topBottomList)
       let option = document.createElement('option')
-      option.value = `${topBottom[i]}`
+      option.value = `${topBottom[i+1]}`
       option.text = `${response.data[topBottom[i]].string}`
       select.append(option)
 
     }
 
+    let selectForm = document.querySelector(".top-bottom-radical-select")
+  selectForm.addEventListener("change", optionValue)
+
+
+  function optionValue(e) {
+    e.preventDefault()
+    let getValue = selectForm.value
+    console.log(getValue)
+    //*****(call new async radical function and pass through getValue)
+    radicalPass(getValue)
+  }
   } catch (error) {
     console.log(`${error}`)
   } finally {
@@ -148,7 +162,6 @@ let topBottomMenu = async () => {
 
 }
 
-// topBottomMenu();
 
 let enclosedMenu = async () => {
   // let url = "http://ccdb.hemiola.com/characters?&fields=string,ktotalstrokes&filters=simplified&big5a"
@@ -163,12 +176,24 @@ let enclosedMenu = async () => {
     for (i = 0; i < enclosed.length; i++) {
       enclosedList.push(response.data[enclosed[i]].string)
       let option = document.createElement('option')
-      option.value = `${enclosed[i]}`
+      option.value = `${enclosed[i+1]}`
       option.text = `${response.data[enclosed[i]].string}`
       select.append(option)
       // console.log(enclosedList)
 
     }
+
+    let selectForm = document.querySelector(".enclosed-radical-select")
+  selectForm.addEventListener("change", optionValue)
+
+
+  function optionValue(e) {
+    e.preventDefault()
+    let getValue = selectForm.value
+    console.log(getValue)
+    //*****(call new async radical function and pass through getValue)
+    radicalPass(getValue)
+  }
 
   } catch (error) {
     console.log(`${error}`)
@@ -178,9 +203,35 @@ let enclosedMenu = async () => {
 
 }
 
-// enclosedMenu();
+
+let radicalPass = async (radicalnum) => {
+  
+  let url = `http://ccdb.hemiola.com/characters/radicals/${radicalnum}?&fields=string,ktotalstrokes,kmandarin,kdefinition&filters=simplified`
+
+  try {
+    let response = await axios.get(url)
+console.log(response.data)
+    function addChars() {
+      for (i = 0; i < response.data.length; i++) {
+        let singleChar = document.createElement('p')
+        singleChar.innerText = response.data[i].string
+        document.querySelector('#character-population').append(singleChar)
+
+      }
+    }
 
 
+    addChars()
+  
+  
+
+
+  } catch (error) {
+    console.log(`${error}`)
+  } finally {
+
+  }
+}
 
 
 // //Put value into the API endpoint and make request 
